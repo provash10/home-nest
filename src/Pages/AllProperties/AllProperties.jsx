93b6b152cfa8e280a5772f3d242e2df604b300a2
Provash <provash20cb@gmail.com>
2025-11-27 22:33:19 +0600

    Navbar design

---
+++ src/Pages/AllProperties/AllProperties.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllProperties = () => {
+    return (
+        <div>
+            <h3>All Properties</h3>
+        </div>
+    );
+};
+
+export default AllProperties;
